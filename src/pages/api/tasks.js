b7ede7e2e5f3fc25c5931a9147b192327e9744af
Lucas de Castro Patrão Rezende Silva <lucasdecastropatrao@gmail.com>
2025-02-12 20:10:30 -0300

    refactor: update tasks.js to handle PUT request methods
    - Handling operations for task updating
    - Implement validation for task input fields

--- src/pages/api/tasks.js
+++ src/pages/api/tasks.js
@@ -5,8 +5,14 @@ export default async function (req, res) {
     const dataRes = await fetch("http://localhost:3001/api/tasks");
     const data = await dataRes.json();
 
+    console.log("data", data);
+
     return res.send(data);
-  } else if (req.method === "POST" || req.method === "PATCH") {
+  } else if (
+    req.method === "POST" ||
+    req.method === "PUT" ||
+    req.method === "PATCH"
+  ) {
     const {
       title,
       description,
@@ -25,25 +31,24 @@ export default async function (req, res) {
         ? "http://localhost:3001/api/tasks"
         : `http://localhost:3001/api/tasks/${id}`;
 
-    if (
-      !title ||
-      !description ||
-      !dueDate ||
-      !priority ||
-      !timeToFinish ||
-      !status ||
-      active === undefined ||
-      !link ||
-      !createdAt
-    ) {
-      console.log("Missing data");
-      return res.status(400).send("There are missing data");
-    }
-
-    console.log(`request method is ${req.method.toLowerCase()}`);
-    console.log(`url is ${url}`);
+    // if (
+    //   !title ||
+    //   !description ||
+    //   !dueDate ||
+    //   !priority ||
+    //   !timeToFinish ||
+    //   !status ||
+    //   active === undefined ||
+    //   !link ||
+    //   !createdAt
+    // ) {
+    //   console.log("Missing data");
+    //   return res.status(400).send("There are missing data");
+    // }
 
     try {
+      console.log(req.method);
+      console.log(url);
       const axiosResponse = await axios[req.method.toLowerCase()](
         url,
         req.body
