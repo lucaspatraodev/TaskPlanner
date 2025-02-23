33e7baeb60206a2615967257fdcc670ed6f4a95b
Lucas de Castro Patrão Rezende Silva <lucasdecastropatrao@gmail.com>
2025-02-23 17:17:33 -0300

    Refactor:
    - Style and Layout changes.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,47 @@
-import React from "react";
-import Panel from "./Panel";
-import { PanelProvider } from "@/context/PanelContext";
+import { React, useState } from "react";
+import Aside from "./Aside/Aside";
+import { NavigationProvider } from "@/context/NavigationContext";
+import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+
+const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const roboto = Roboto({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const Layout = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
   return (
-    <PanelProvider>
-      <div className="flex flex-col w-full h-[100%] overflow-hidden ">
-        <main className="flex items-center justify-center gap-8 flex-grow min-w-full min-h-full bg-[#000000] overflow-y-auto p-6">
-          <section className="h-full w-3/12 bg-[#454545] rounded-md p-4">
-            <Panel />
-          </section>
-          <section className="h-full w-9/12 bg-[#454545] rounded-md p-4">
+    <NavigationProvider>
+      <div
+        className={`flex w-full h-full bg-[#262626] max-[500px]:bg-[#000000] ${inter.className}`}
+      >
+        <div className="w-[98%] h-full flex flex-row gap-4 mx-auto">
+          <Aside
+            onMenuChange={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}
+          />
+          <main
+            className={`${
+              isMenuOpen ? "md:w-[80%] lg:w-[82%]" : "w-[100%]"
+            } flex flex-col gap-[20px] h-[94%] my-auto bg-[#000000] rounded-xl p-[12px] py-[20px] ${
+              roboto.className
+            }`}
+          >
             {children}
-          </section>
-        </main>
+          </main>
+        </div>
       </div>
-    </PanelProvider>
+    </NavigationProvider>
   );
 };
 
